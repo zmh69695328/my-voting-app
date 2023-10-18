bd0a60dc09c7736ed5578dff8792d45f9eb0d2bd
zmh69695328 <736503352@qq.com>
2023-10-18 14:11:30 +0800

    feat:Add backend-go repo

---
+++ backend-go/cmd/api-server/main.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"net/http"
+
+	"github.com/labstack/echo/v4"
+	"github.com/labstack/echo/v4/middleware"
+	"github.com/zmh69695328/voting-backend-go/internal/model/sqlite"
+)
+
+func main() {
+	// Echo instance
+	e := echo.New()
+
+	// Middleware
+	e.Use(middleware.Logger())
+	e.Use(middleware.Recover())
+
+	// Routes
+	e.GET("/", hello)
+
+	// Start server
+	e.Logger.Fatal(e.Start(":1323"))
+}
+
+// Handler
+func hello(c echo.Context) error {
+	sqlite.SQLiteDemo()
+	return c.String(http.StatusOK, "Hello, World!")
+}
---
+++ backend-go/internal/model/sqlite/sqlite.go
@@ -0,0 +1,39 @@
+package sqlite
+
+import (
+	"database/sql"
+	"fmt"
+	"strconv"
+	"time"
+
+	_ "github.com/mattn/go-sqlite3"
+)
+
+func SQLiteDemo() {
+	var start = time.Now().UnixMicro()
+	database, err := sql.Open("sqlite3", fmt.Sprintf("./alpha%d.db", start))
+	if nil != err {
+		fmt.Println(err)
+	}
+	stmt, _ := database.Prepare("create table if not exists user(id integer primary key, firstname text, lastname text)")
+	stmt.Exec()
+
+	stmt, _ = database.Prepare("insert into user( firstname, lastname) values(?,?)")
+	stmt.Exec("Jack", "Chen")
+
+	var id int
+	var firstname string
+	var lastname string
+	rows, err := database.Query("select id, firstname, lastname from user")
+	if nil != err {
+		fmt.Println(err)
+	}
+	for rows.Next() {
+		rows.Scan(&id, &firstname, &lastname)
+		fmt.Println(strconv.Itoa(id) + ": " + firstname + " " + lastname)
+	}
+
+	var end = time.Now().UnixMicro()
+	fmt.Printf("SQLiteDemo: %d\n", end-start)
+
+}
