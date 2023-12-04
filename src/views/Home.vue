daaf42ac9cad91fbe1e5943d9a955b5628fb3831
zmh69695328 <736503352@qq.com>
2023-12-04 16:19:15 +0800

    chore:update class

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -17,7 +17,7 @@ watch(
     )
 </script>
 <template>
-    <div v-show="flag===true" role="alert" class="alert absolute inset-x-0 top-10 w-2/3 center mx-auto">
+    <div v-show="flag===true" role="alert" class="alert absolute inset-x-0 top-12 w-2/3 center mx-auto">
         <!-- <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" class="stroke-info shrink-0 w-6 h-6">
             <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
                 d="M13 16h-1v-4h-1m1-4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"></path>
